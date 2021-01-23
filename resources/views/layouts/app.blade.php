e07d9247d308f26d83b5a151dc9de1b02e5e5394
takaki <cheap_trick_magic@yahoo.co.jp>
2021-01-23 15:59:59 +0900

    フラッシュメッセージの実装

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -84,6 +84,12 @@
 
 
         <main class="main">
+            @if (session('flash_message'))
+            <div class="flash_message bg-success text-center py-3 my-0 mb30">
+                {{ session('flash_message') }}
+            </div>
+            @endif
+
             @yield('content')
         </main>
 
